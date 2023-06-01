1c590d3a4ab0a23d2bbb94d7eb7e7c843b46d9dc
mendesisis <vantaeisis@gmail.com>
2023-05-31 21:24:54 -0300

    SALVO

--- App.tsx
+++ App.tsx
@@ -1,10 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import * as Font from 'expo-font';
 
 export default function App() {
+  const [fonteCarregada, setFonteCarregada] = useState(false);
+  
+
+  async function loadFonts() {
+    await Font.loadAsync({
+     playfairdisplay_regular: require('./assets/Playfair_Display/static/PlayfairDisplay-Regular.ttf'),
+     playfairdisplay_italic: require('./assets/Playfair_Display/static/PlayfairDisplay-Italic.ttf'),
+     playfairdisplay_black: require('./assets/Playfair_Display/static/PlayfairDisplay-Black.ttf')
+    });
+    
+  }
+
+  useEffect(()=>{
+    loadFonts()
+    .then (()=>{
+      setFonteCarregada(true);
+    })
+    .catch((error)=>{
+      console.log('Ocorreu um erro', error);
+    })
+
+
+  }, []);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableHighlight
+      style={{width:130,
+      borderRadius: 4,
+      backgroundColor: 'black',
+      flexDirection:'row',
+      justifyContent:'center',
+      alignItems:'center',
+      height: 40}}>
+      
+     
+      {fonteCarregada ?
+      <Text style={{fontFamily:'playfairdisplay_regular', fontSize: 50}}>Click!</Text>
+      :
+      <Text></Text>
+     }   
+        </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
   );
